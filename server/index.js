1e3b0fcb898ef3096ed981e71dac9bdc3a3eafe0
Ji Park <jiyongprk@gmail.com>
2022-06-03 15:22:43 -0400

    add delete function

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(cors())
 
 mongoose.connect('mongodb+srv://norf:norf@cluster0.71y4lk4.mongodb.net/users?retryWrites=true&w=majority')
 
+//read
 app.get('/', async(req,res)=>{
     UserModel.find({},(err,result)=>{
         try {
@@ -21,11 +22,18 @@ app.get('/', async(req,res)=>{
     })
 })
 
+//create(post)
 app.post('/newuser', async (req,res)=>{
     const addNewUser = req.body
     const newUser = new UserModel(addNewUser)
     await newUser.save()
     res.json(addNewUser)
+})
+
+//delete 
+app.delete('/deleteuser/:id', async (req,res)=>{
+    const id = req.params.id
+    await UserModel.findByIdAndRemove(id).exec()
 })
 
 app.listen(3001,()=>{console.log('connect to port 3001')})
